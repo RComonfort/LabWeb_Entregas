69793359e3815973a9b913fd8c5aab4ebb660fdf
Erreseve <a.comonfort@outlook.com>
2018-02-12 17:09:03 -0600

    css and jquery not fucking loadgindiahfkqhfkj

--- examenParcial1/server.js
+++ examenParcial1/server.js
@@ -1,9 +1,14 @@
 var express = require ('express');
 var PORT = 8083;
 
+var routes = require('./css');
+var routes = require('./js');
+var routes = require('./app');
+
 var app=express();
 app.get ('/', function(req, res)
 {
+    //app.use('/public', express.static(__dirname + '/public'));
     res.sendfile ('index.html')
 });
 
